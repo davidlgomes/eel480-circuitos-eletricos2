f59636ac7135af7543c1c4dcdba88ce0f0bd02a8
David Lopes Gomes <gomes.david.9@gmail.com>
2024-05-29 19:56:43 -0300

    Delete trab1davidgomes.py

--- trab1davidgomes.py
+++
@@ -1,9 +0,0 @@
-from dglendonetlist import lendoNetlist
-from dgcalculartensaonodal import calcularTensaoNodal
-def main(arqNetlist):
-    listaComponentes = lendoNetlist(arqNetlist)
-    quantidadeNos = max(max(componente[1], componente[2]) for componente in listaComponentes)
-    tensaoNodal = calcularTensaoNodal(listaComponentes, quantidadeNos + 1)  # Adiciona o nó de terra
-    for i, tensaoNodal in enumerate(tensaoNodal):
-        print(f'e{i + 1} = {tensaoNodal:.15f} V')
-    return ''
